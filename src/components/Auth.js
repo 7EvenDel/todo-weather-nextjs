c107eea80a48cb1b00d4d3a628469b02b3372ed4
culmination <ovsiichuk.bohdan@gmail.com>
2024-03-22 19:58:07 +0200

    some visual

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -12,16 +12,25 @@ import { Checkbox } from "./ui/checkbox";
 import Image from "next/image";
 import { signIn } from "next-auth/react";
 
-const schema = z.object({
-  name: z.string().min(3, { message: "Ім'я має містити щонайменше 3 символи" }),
-  contacts: z
-    .string()
-    .min(6, { message: "Контакти мають містити щонайменше 6 символів" }),
-  comment: z.string().optional(),
-});
-
 const Auth = () => {
   const [sending, setSending] = useState(false);
+  const [login, setLogin] = useState(true);
+
+  const schema = login
+    ? z.object({
+        email: z.string().min(1, { message: "You forgot to enter your email" }),
+        password: z
+          .string()
+          .min(6, { message: "Password must contain at least 6 characters" }),
+      })
+    : z.object({
+        firstname: z.string().min(1, { message: "Enter your first name" }),
+        lastname: z.string().min(1, { message: "Enter your last name" }),
+        email: z.string().min(1, { message: "You forgot to enter your email" }),
+        password: z
+          .string()
+          .min(6, { message: "Password must contain at least 6 characters" }),
+      });
 
   const {
     register,
@@ -32,81 +41,109 @@ const Auth = () => {
   });
 
   const onSubmit = async (data) => {
-    setSending(true);
-    var date = new Date();
-    data.formattedDate = `${date.getDate()}-${
-      date.getMonth() + 1
-    }-${date.getFullYear()}`;
+    // setSending(true);
+    console.log(data);
 
-    try {
-      const res = await fetch("http://localhost:3000/api/auth", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+    // try {
+    //   const res = await fetch("http://localhost:3000/api/auth", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(data),
+    //   });
 
-      if (!res.ok) {
-        openPopup("Data wasn't sent");
-        setSending(false);
-        throw new Error("failed to fetch");
-      }
+    //   if (!res.ok) {
+    //     // openPopup("Data wasn't sent");
+    //     setSending(false);
+    //     throw new Error("failed to fetch");
+    //   }
 
-      setSending(false);
-      openPopup("Ваші дані успішно надіслані!");
-      return res.text();
-    } catch (error) {
-      openPopup("Не вдалося надіслати...");
-      setSending(false);
-    }
+    //   setSending(false);
+    //   openPopup("Ваші дані успішно надіслані!");
+    //   return res.text();
+    // } catch (error) {
+    //   openPopup("Не вдалося надіслати...");
+    //   setSending(false);
+    // }
   };
 
   return (
     <section className="flex flex-col items-center mt-[103px] bg-cover">
-      <div className="w-[554px] h-[669px]">
-        <h1 className="font-bold text-[32px]">Create account</h1>
-        <p className="text-[16px] mb-[100px]">
-          Already have an accont?
-          <Button variant="link" className="text-[16px] font-normal -mx-2">
-            Login
-          </Button>
-        </p>
-        <form onSubmit={handleSubmit(onSubmit)} className="w-full">
-          <div className="mb-5 flex gap-8 w-full">
-            <div className="w-1/2">
-              <Input
-                type="text"
-                {...register("name")}
-                placeholder="First Name"
-                className="text-[16px] px-4 py-3 bg-transparent border border-gray-500 rounded w-full h-12"
-              />
-              {errors.name && (
-                <p className="text-red-500 text-sm">{errors.name.message}</p>
-              )}
+      <div className="w-[554px] h-[669px] flex flex-col">
+        {login ? (
+          <>
+            <h1 className="font-bold text-[32px]">Welcome Back</h1>
+            <p className="text-[16px] mb-[100px]">
+              {`Don't have an account?`}
+              <Button
+                variant="link"
+                className="text-[16px] font-normal -mx-2"
+                onClick={() => setLogin((login) => !login)}
+              >
+                Sign up
+              </Button>
+            </p>
+          </>
+        ) : (
+          <>
+            <h1 className="font-bold text-[32px]">Create account</h1>
+            <p className="text-[16px] mb-[100px]">
+              Already have an accont?
+              <Button
+                variant="link"
+                className="text-[16px] font-normal -mx-2"
+                onClick={() => setLogin((login) => !login)}
+              >
+                Login
+              </Button>
+            </p>
+          </>
+        )}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-full flex flex-col"
+        >
+          {login ? null : (
+            <div className="mb-5 flex gap-8 w-full">
+              <div className="w-1/2">
+                <Input
+                  type="text"
+                  {...register("firstname")}
+                  placeholder="First Name"
+                  className="text-[16px] px-4 py-3 bg-transparent border border-gray-500 rounded w-full h-12"
+                />
+                {errors.firstname && (
+                  <p className="text-red-500 text-sm">
+                    {errors.firstname.message}
+                  </p>
+                )}
+              </div>
+              <div className="w-1/2">
+                <Input
+                  type="text"
+                  {...register("lastname")}
+                  placeholder="Last Name"
+                  className="text-[16px] px-4 py-3 bg-transparent border border-gray-500 rounded w-full h-12"
+                />
+                {errors.lastname && (
+                  <p className="text-red-500 text-sm">
+                    {errors.lastname.message}
+                  </p>
+                )}
+              </div>
             </div>
-            <div className="w-1/2">
-              <Input
-                type="text"
-                {...register("name")}
-                placeholder="Last Name"
-                className="text-[16px] px-4 py-3 bg-transparent border border-gray-500 rounded w-full h-12"
-              />
-              {errors.name && (
-                <p className="text-red-500 text-sm">{errors.name.message}</p>
-              )}
-            </div>
-          </div>
+          )}
 
           <div className="mb-5">
             <Input
               type="text"
               placeholder="Email"
-              {...register("contacts")}
+              {...register("email")}
               className="text-[16px] px-4 py-3 bg-transparent border border-gray-500 rounded w-full h-12"
             />
-            {errors.contacts && (
-              <p className="text-red-500 text-sm">{errors.contacts.message}</p>
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
             )}
           </div>
 
@@ -114,27 +151,40 @@ const Auth = () => {
             <Input
               type="text"
               placeholder="Password"
-              {...register("contacts")}
+              {...register("password")}
               className="text-[16px] px-4 py-3 bg-transparent border border-gray-500 rounded w-full h-12"
             />
-            {errors.contacts && (
-              <p className="text-red-500 text-sm">{errors.contacts.message}</p>
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
             )}
           </div>
 
-          <div className="flex gap-2 items-center mb-6">
-            <Checkbox className="size-[18px] rounded border-black" />
-            <Label className="text-[14px]">
-              I agree to DopeSass{" "}
-              <Button variant="link" className="p-0 py-0 h-4" type="button">
-                Terms of service
-              </Button>{" "}
-              and{" "}
-              <Button variant="link" className="p-0 py-0 h-4" type="button">
-                Privacy policy
-              </Button>
-            </Label>
-          </div>
+          {login ? (
+            <Button
+              variant="link"
+              type="button"
+              className="p-0 h-4 mb-6 self-end"
+            >
+              Forgot Password
+            </Button>
+          ) : (
+            <div className="flex gap-2 items-center mb-6">
+              <Checkbox
+                className="size-[18px] rounded border-black"
+                // onCheckedChange={() => setSending(true)}
+              />
+              <Label className="text-[14px]">
+                I agree to DopeSass{" "}
+                <Button variant="link" className="p-0 py-0 h-4" type="button">
+                  Terms of service
+                </Button>{" "}
+                and{" "}
+                <Button variant="link" className="p-0 py-0 h-4" type="button">
+                  Privacy policy
+                </Button>
+              </Label>
+            </div>
+          )}
 
           <Button
             type="submit"
@@ -145,7 +195,7 @@ const Auth = () => {
                 : "w-full h-16 flex items-center justify-center cursor-pointer flex h-[49px] text-[16px] mb-6"
             }
           >
-            <p>{sending ? "Wait..." : "Create account"}</p>
+            <p>{sending ? "Wait..." : login ? "Log in" : "Create Account"}</p>
           </Button>
 
           <div className="flex items-center justify-between mb-6">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,48 @@
 import Link from "next/link";
 import { Label } from "./ui/label";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 import Image from "next/image";
+import { Info, InfoIcon } from "lucide-react";
+import { useSession, signOut } from "next-auth/react";
 
 const Header = () => {
+  const { data: session } = useSession();
+  const userprofile =
+    session && session.user
+      ? session.user
+      : {
+          name: "name",
+          image: "https://cdn-icons-png.flaticon.com/256/848/848043.png",
+          email: "email",
+        };
+
   return (
     <header className="bg-white drop-shadow pb-2">
       <div className="container flex justify-between items-center">
@@ -13,6 +53,148 @@ const Header = () => {
           <Image src="/logo.jpg" alt="logo" width={50} height={50} />
           <Label>ToDoWeath</Label>
         </Link>
+        <Dialog>
+          <DialogTrigger asChild className="mt-2 relative size-12">
+            <img
+              src={userprofile.image}
+              alt="user"
+              className="size-12 rounded-full cursor-pointer hover-opacity-0"
+            />
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle className="flex items-center gap-2">
+                Edit profile{" "}
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <InfoIcon className="size-4 text-gray-700" />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>
+                        Your account`s data can be used by DopeSass Terms of
+                        service and Privacy policy
+                      </p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here
+              </DialogDescription>
+              <img
+                src={userprofile.image}
+                alt="user"
+                className="size-12 rounded-full absolute right-10 top-3"
+              />
+            </DialogHeader>
+            <Input
+              id="name"
+              defaultValue={userprofile.name}
+              className="w-full"
+            />
+            <Input
+              id="username"
+              defaultValue={userprofile.email}
+              className="w-full"
+            />
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Language" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Select your language</SelectLabel>
+                  <SelectItem value="ukrainian">Ukrainian</SelectItem>
+                  <SelectItem value="english">English</SelectItem>
+                  <SelectItem value="italian">Italian</SelectItem>
+                  <SelectItem value="germany">Germany</SelectItem>
+                  <SelectItem value="french">French</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <div className="flex gap-2 w-full">
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Date Format" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select date format</SelectLabel>
+                    <SelectItem value="ukrainian">MM/DD/YYYY</SelectItem>
+                    <SelectItem value="english">DD/MM/YYYY</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>{" "}
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Time Format" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select time format</SelectLabel>
+                    <SelectItem value="ukrainian">12h (am/pm)</SelectItem>
+                    <SelectItem value="english">24h</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Country" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Select your country</SelectLabel>
+                  <SelectItem value="ukraine">Ukraine</SelectItem>
+                  <SelectItem value="usa">United States</SelectItem>
+                  <SelectItem value="italy">Italy</SelectItem>
+                  <SelectItem value="germany">Germany</SelectItem>
+                  <SelectItem value="france">France</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Time Zone" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Select your country</SelectLabel>
+                  <SelectItem value="uscanada">
+                    Central Time - US & Canada
+                  </SelectItem>
+                  <SelectItem value="uscanada1">
+                    Central Time - US & Canada
+                  </SelectItem>
+                  <SelectItem value="uscanada2">
+                    Central Time - US & Canada
+                  </SelectItem>
+                  <SelectItem value="uscanada3">
+                    Central Time - US & Canada
+                  </SelectItem>
+                  <SelectItem value="uscanada4">
+                    Central Time - US & Canada
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <DialogFooter className="flex w-full justify-between">
+              <DialogClose>
+                <Button type="submit">Save changes</Button>
+              </DialogClose>
+              <DialogClose>
+                <Button
+                  type="submit"
+                  variant="destructive"
+                  onClick={() => signOut()}
+                >
+                  Sign out
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
     </header>
   );
